77b7274732f4b7007afb82a0f89a636dab8787b4
Adrià Gomis <gomisroca@gmail.com>
2026-05-14 08:42:17 +0200

    allow all image hosts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['cdn.discordapp.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
 };
 
