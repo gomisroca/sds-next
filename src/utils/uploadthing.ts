17fddf5416fe8768e8776e3c8c3c86786d7a4ace
Adrià Gomis <gomisroca@gmail.com>
2026-05-15 16:48:19 +0200

    rename files before upload

--- src/utils/uploadthing.ts
+++ src/utils/uploadthing.ts
@@ -1,12 +1,16 @@
-import {
-  generateReactHelpers,
-  generateUploadButton,
-  generateUploadDropzone,
-} from "@uploadthing/react";
+import { generateReactHelpers, generateUploadButton, generateUploadDropzone } from '@uploadthing/react';
+import { v4 as uuidv4 } from 'uuid';
 
-import type { UploadThingRouter } from "@/app/api/uploadthing/core";
+import type { UploadThingRouter } from '@/app/api/uploadthing/core';
 
 export const UploadButton = generateUploadButton<UploadThingRouter>();
 export const UploadDropzone = generateUploadDropzone<UploadThingRouter>();
-export const { useUploadThing, uploadFiles } =
-  generateReactHelpers<UploadThingRouter>();
+export const { useUploadThing, uploadFiles } = generateReactHelpers<UploadThingRouter>();
+
+export function renameFiles(files: File[]) {
+  return files.map((file) => {
+    const ext = file.name.split('.').pop();
+    const newFileName = `${uuidv4()}.${ext}`;
+    return new File([file], newFileName, { type: file.type });
+  });
+}
